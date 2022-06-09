2eaf63cf34bcd9f21bc94167acd48cb13ad3347d
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-09 18:09:22 -0400

    UI setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # password-manager
- Simple Python password manager
+ A simple python password manager using Tkinter.
+
+
+Resources: 
+
+Graphical User Interfaces with Tk:
+https://docs.python.org/3/library/tk.html
+
+Password Rules Are Bullshit - Coding Horrors
+https://blog.codinghorror.com/password-rules-are-bullshit/
---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+LIGHT = "#ffffff"
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg=LIGHT)
+
+canvas= Canvas(width=200, height=200, bg=LIGHT, highlightthickness=1)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 112, image=logo_img)
+
+window.mainloop()
