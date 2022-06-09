36f6e785b551234490034333433b441791cf5a85
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-09 18:43:07 -0400

    ui setup - fix alignment issues

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ LIGHT = "#ffffff"
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas= Canvas(width=200, height=200, highlightthickness=2)
 logo_img = PhotoImage(file="logo.png")
@@ -28,7 +28,7 @@ website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-password_entry = Entry(width=21)
+password_entry = Entry(width=18)
 password_entry.grid(row=3, column=1)
 
 #Buttons
