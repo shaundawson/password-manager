3cafc0834eb804687cee09b1b896c727faf0930d
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-09 18:41:09 -0400

    add buttons

--- main.py
+++ main.py
@@ -27,9 +27,15 @@ password_label.grid(row=3, column=0)
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
+email_entry.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
+
+#Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 
