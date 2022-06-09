39ba4e027890715e8c18274d2e8cddc018d69655
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-09 18:35:52 -0400

    ui setup - add entries

--- main.py
+++ main.py
@@ -16,12 +16,20 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 #Labels
-website_label = Label(text="Website")
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Emacolil/Username")
+email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password")
+password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
+
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 
 
