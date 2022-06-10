cd59a6ff557d5f4c02b43f73183c50d54e56fff3
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-10 10:48:36 -0400

    focus cursor on website_entry

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ password_label.grid(row=3, column=0)
 #Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=18)
@@ -36,7 +37,5 @@ generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36)
 add_button.grid(row=4, column=1, columnspan=2)
-
-
 
 window.mainloop()
