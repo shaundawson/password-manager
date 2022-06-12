6081fdf8ae937273eb7cdde810e64f27aeeb04ae
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-12 09:52:04 -0400

    add error handling

--- main.py
+++ main.py
@@ -30,16 +30,18 @@ def add_password():
     email = email_entry.get()
     password = password_entry.get()
     
+
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok =  messagebox.askokcancel(title=website, message=f"These are the details entered: \n Email: {email} \n Password: {password}\n Is it ok to save?")
-
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                      f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
             with open("data.txt", "a") as data_file:
                 data_file.write(f"{website} | {email} | {password}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
+
          
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
